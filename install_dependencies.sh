4eab54ab59c8651c830201838df195897e97b427
Gareth Ellis <gareth.ellis0@gmail.com>
2017-01-14 13:28:17 -0800

    Added libserial as a dependency

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -15,6 +15,7 @@ echo "Installing other ROS dependencies..."
 echo "================================================================"
 
 sudo apt-get install -y\
+    libserial0 \
     ros-kinetic-xacro \
     ros-kinetic-controller-manager \
     ros-kinetic-gazebo-ros-control \
